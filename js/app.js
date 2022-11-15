6a9847239cc4db663bf19d88f903a06536851cef
Vixuvious <118066058+Vixuvious@users.noreply.github.com>
2022-11-14 22:03:16 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 "use strict";
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode#changes_in_strict_mode
 
 // defines an element called button
+//mdn javascript object
+//https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model
 let button = document.getElementById('button');
+//https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
 button.addEventListener('click', click);
 
 //creates a function called click
