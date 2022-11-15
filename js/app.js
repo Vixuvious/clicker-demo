3bd7a8d8e6f6a343f1c9d31819834a392a13edbf
Vixuvious <118066058+Vixuvious@users.noreply.github.com>
2022-11-14 23:30:37 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,29 @@
-"use strict";
+//"use strict";
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode#changes_in_strict_mode
 
 // defines an element called button
 //mdn javascript object
 //https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model
-let button = document.getElementById('button');
+// let button = document.getElementById('button');
 
 //https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
-button.addEventListener('click', click);
+// button.addEventListener('click', click);
 //https://www.youtube.com/watch?v=XF1_MlZ5l6M&t=724s
 
 //creates a function called click with the paramater 'e'. Later an argument will be given to fill in for the parameter
+// function click(e) {
+//   console.log('button was clicked');
+//   let number = document.getElementById("number");
+//   number.innerText = Number(number.innterText) + 1;
+// }
+
+"use strict";
+
+let button = document.getElementById('button');
+button.addEventListener('click', click);
+
 function click(e) {
   console.log('button was clicked');
   let number = document.getElementById("number");
-  number.innerText = Number(number.innterText) + 1;
+  number.innerText = Number(number.innerText) + 1;
 }
