56349d0f752a22f3a0bf87c00717cc7990dece4f
Vixuvious <118066058+Vixuvious@users.noreply.github.com>
2022-11-14 23:18:33 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,11 +6,11 @@
 //https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model
 let buttonA = document.getElementById('button1');
 //https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
-button.addEventListener('click', click);
+buttonA.addEventListener('click', clicked);
 //https://www.youtube.com/watch?v=XF1_MlZ5l6M&t=724s
 
 //creates a function called click with the paramater 'e'. Later an argument will be given to fill in for the parameter
-function click(e) {
+function clicked(e) {
   console.log('button was clicked');
   let number = document.getElementById("number");
   number.innerText = Number(number.innterText) + 1;
