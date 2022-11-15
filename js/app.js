359f74c1911e805a45bfc8d00260afea5dc11cae
Vixuvious <118066058+Vixuvious@users.noreply.github.com>
2022-11-14 23:21:15 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,14 +4,14 @@
 // defines an element called button
 //mdn javascript object
 //https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model
-let buttonA = document.getElementById('button1');
+let button = document.getElementById('button');
 //https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
-buttonA.addEventListener('click', clicked);
+button.addEventListener('click', clicked);
 //https://www.youtube.com/watch?v=XF1_MlZ5l6M&t=724s
 
 //creates a function called click with the paramater 'e'. Later an argument will be given to fill in for the parameter
 function clicked(e) {
   console.log('button was clicked');
-  let number = document.getElementById("number1");
+  let number = document.getElementById("number");
   number.innerText = Number(number.innterText) + 1;
 }
