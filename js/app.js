f273104b39d0ecba950dfeca78c125ae846ca460
Vixuvious <118066058+Vixuvious@users.noreply.github.com>
2022-11-14 21:29:13 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 "use strict";
 
+// defines an element called button
 let button = document.getElementById('button');
 button.addEventListener('click', click);
 
+//creates a function called click
 function click(e) {
   console.log('button was clicked');
   let number = document.getElementById("number");
